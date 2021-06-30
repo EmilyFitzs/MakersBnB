8e41013db3061142d69fe93d33c2dd7c607229f2
ehelsan <ehelsan@hotmail.co.uk>
2021-06-30 12:11:07 +0100

    advertise properties complete

--- lib/bnb.rb
+++ lib/bnb.rb
@@ -1,4 +1,6 @@
 require 'pg'
+require_relative 'database_connection'
+require './database_connection_setup'
 
 class Bnb
   def self.all
@@ -8,9 +10,12 @@ class Bnb
       connection = PG.connect(dbname: 'makersbnb')
     end
     properties = connection.exec('SELECT * FROM properties;')
-    properties.map { |row|  
-      {"name"=>row['name'], "description"=>row['description'], "price"=>row['price']}
-    }
+    properties.map do |row|
+      Bnb.new(name: row['name'], description: row['description'], price: row['price'])
+    end
+    # { |row|  
+    #   {'name: '=>row['name'], 'description: '=>row['description'], 'price: '=>row['price']}
+    # }
   end
 
   def self.create(name:, description:, price:)
@@ -19,6 +24,21 @@ class Bnb
     else
       connection = PG.connect(dbname: 'makersbnb')
     end
-    connection.exec("INSERT INTO properties (name, description, price) VALUES('#{name}', '#{description}', '#{price}')")
+
+    rs = connection.exec("INSERT INTO properties (name, description, price) VALUES('#{name}', '#{description}', '#{price}') RETURNING name, description, price;")
+    Bnb.new(name: rs[0]['name'], description: rs[0]['description'], price: rs[0]['price'])
   end
+
+  def self.truncate
+    DatabaseConnection.query("TRUNCATE TABLE properties")
+  end
+
+  attr_reader :name, :description, :price
+
+  def initialize(name:, description:, price:)
+    @name  = name
+    @description = description
+    @price = price
+  end
+
 end
