f490d25c094d67eebe60c08c42bf070a543fa5ba
Emily <emily.fitzsimmonsx@gmail.com>
2021-07-02 15:03:09 +0100

    styling for add properties page

--- controllers/user_controller.rb
+++ controllers/user_controller.rb
@@ -4,25 +4,29 @@ class UserController < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end
-  
   set :views, Proc.new { File.join(APP_ROOT, "views") }
+
+  get '/properties' do
+    @properties = Bnb.all
+    erb :properties
+  end
 
   get '/signup' do
     erb :"sessions/sign_up"
   end
 
   post '/signup' do
-    if User.find_column(column_name: "email", value: params[:email])
-      flash[:notice] = "A user already exists with this email"
-      redirect('/signup')
-    elsif params[:password_confirmation] == params[:password]
-      user = User.create(email: params[:email], password: params[:password])
-      session[:user_id] = user.id
-      redirect('/')
-    else
-      flash[:notice] = "Passwords don't match"
-      redirect('/signup')
-    end
+   if User.find_column(column_name: "email", value: params[:email])
+    flash[:notice] = "A user already exists with this email"
+    redirect('/signup')
+   elsif params[:password_confirmation] == params[:password]
+     user = User.create(email: params[:email], password: params[:password])
+     session[:user_id] = user.id
+     redirect('/')
+   else
+    flash[:notice] = "Passwords don't match"
+    redirect('/signup')
+   end
   end
 
   get '/' do
@@ -37,8 +41,8 @@ class UserController < Sinatra::Base
   post '/sessions' do
     user = User.authenticate(email: params[:email], password: params[:password])
     if user
-      session[:user_id] = user.id
-      redirect('/')
+    session[:user_id] = user.id
+    redirect('/')
     else 
       flash[:notice] = "Username or password is incorrect"
       redirect ('/signin')
@@ -49,5 +53,10 @@ class UserController < Sinatra::Base
     check_authenticated(session: session)
     session.clear
     redirect('/signin')
+  end
+
+  get '/properties' do
+    @bnb = Bnb.all
+    erb(:properties)
   end
 end
